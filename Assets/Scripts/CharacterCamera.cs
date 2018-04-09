4090de88aacb34a1f794ee702e61ea56bde74119
Michail Michail <michaelmich3@hotmail.com>
2018-04-09 15:38:46 -0500

    3D Modular Assets, Custom First Person Script, Post Processing

---
+++ Assets/Scripts/CharacterCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterCamera : MonoBehaviour
+{
+    [SerializeField] private float sensitivityX = 2.5f;
+    [SerializeField] private float sensitivityY = 2;
+    [SerializeField] private float smoothing = 2;
+    [SerializeField] private float minAngle = -80;
+    [SerializeField] private float maxAngle = 90;
+
+    GameObject character;
+    Vector2 mouseLook;
+    Vector2 smoothV;
+
+    private void Start()
+    {
+        character = this.transform.parent.gameObject;
+    }
+
+    private void Update()
+    {
+        var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        md = Vector2.Scale(md, new Vector2(sensitivityX * smoothing, sensitivityY * smoothing));
+        smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
+        mouseLook += smoothV;
+        mouseLook.y = Mathf.Clamp(mouseLook.y, minAngle, maxAngle);
+
+        transform.localRotation = Quaternion.AngleAxis(-mouseLook.y, Vector3.right);
+        character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+    }
+}
---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 5;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float straffe = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        straffe *= Time.deltaTime;
+
+        transform.Translate(straffe, 0, translation);
+
+        if (Input.GetKeyDown("escape"))
+            Cursor.lockState = CursorLockMode.None;
+    }
+}
