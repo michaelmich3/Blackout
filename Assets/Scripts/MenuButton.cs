b621671ed1e428bdaf48f3bbbd962158806f94c9
michaelmich3 <michaelmich3@hotmail.com>
2018-05-07 10:50:19 -0500

    Added hallways, room 2, room 3, main menu

---
+++ Assets/Scripts/MenuButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void ShowObject(GameObject objectToShow)
+    {
+        objectToShow.SetActive(true);
+    }
+
+    public void HideObject(GameObject objectToHide)
+    {
+        objectToHide.SetActive(false);
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
