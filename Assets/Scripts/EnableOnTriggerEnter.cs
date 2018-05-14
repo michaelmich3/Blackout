0f7b64ec66feb35d8ba21a72cfb4ffc858c23aa4
michaelmich3 <michaelmich3@hotmail.com>
2018-05-13 19:36:46 -0500

    Small fixes on the main level, Removed ProBuilder (it wasn't being used)

---
+++ Assets/Scripts/EnableOnTriggerEnter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableOnTriggerEnter : MonoBehaviour
+{
+    [SerializeField] private Collider trigger;
+    [SerializeField] private GameObject objectToActivate;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            EnableObject();
+        }
+    }
+
+    private void EnableObject()
+    {
+        objectToActivate.SetActive(true);
+    }
+}
